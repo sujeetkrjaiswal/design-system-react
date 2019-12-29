21c61bb5d7e2679e00f2ed388b00d94aa7cab9d9
Sujeet Kumar Jaiswal <sujeet.j@media.net>
2019-12-29 23:54:22 +0530

    test setup done

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'src/**/*.ts?(x)': 'eslint --fix',
+  'src/**/*.scss': 'stylelint --fix',
+  // Not required for the current project
+  // 'src/**/*.js?(x)': 'eslint --fix',
+  // 'src/**/*.css': 'stylelint --fix'
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+};
